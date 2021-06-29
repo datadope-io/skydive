2a0f29ed7120c5823870ec6a03e2feba2eea539b
Adrian Lopez <adrianlzt@gmail.com>
2021-06-29 11:05:05 +0200

    events step: concurrencia y definición de tiempo opcional
    Se implementa concurrencia a la hora de obtener todas las revisiones de
    los nodos para un ID determinado.
    De esta manera aumentamos la velocidad de respuesta de skydive al
    procesar este step.
    Anteriomente, se realizaban todas las peticiones al backend
    (elasticsearch) de manera secuencial.
    Ahora, usando un canal como semáforo, permitimos 40 peticiones
    concurrentes a elasticsearch.
    Este número se ha elegido tras hacer varias pruebas con distintos
    valores usando un entorno local.
    Si intentamos enviar demasiadas peticiones concurrentes, ES se puede
    saturar y tardar más en contestar (comparado con todo secuencial).
    40 se ha encontrado que es un valor razonable de concurrencia, que no
    satura a ES y aumenta unas 10 veces la velocidad del step.
    
    También se añade la posibilidad de definir el tiempo del que queremos
    obtener los eventos en el propio step.
    Hasta ahora la query a utilizar era del tipo:
    G.At(1624957746, 3600).V().Events('foo', 'agg')
    
    Esta query impone un sobrecoste a los steps intermedios, que tienen que
    obtener la información del backend.
    Con el nuevo esquema:
    G.V().Events('foo', 'agg', 1624940000, 1624957746)
    Lo que hacemos es obtener una serie de nodos, y al final, pedir que para
    cada uno de esos nodos obtenga y agrupe los eventos entre un startTime y
    endTime.
    El problema de esta aproximación es que estamos obteniendo el grafo de
    nodos actual y luego los eventos pasados.
    Podría darse el caso de que haya noos que hayan sido borrados, pero si
    estuviesen en el momento que queremos los eventos.
    Con esta aproximación, esos nodos no aparecerían.
    O de manera similar, puede que se retornen nodos que no existían en el
    periodo del que queremos los eventos.

--- gremlin/traversal/events.go
+++ gremlin/traversal/events.go
@@ -2,6 +2,8 @@ package traversal
 
 import (
 	"reflect"
+	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/skydive-project/skydive/graffiti/graph"
@@ -14,11 +16,23 @@ type EventsTraversalExtension struct {
 	EventsToken traversal.Token
 }
 
-// EventsGremlinTraversalStep describes the Events gremlin traversal step
+// EventsGremlinTraversalStep step aggregates events from different revisions of the nodes into a new metadata key.
+// This step should be used with a presistant backend, so it can access previous revisions of the nodes.
+// To use this step we should select a metadata key (first parameter), where the events will be read from.
+// Inside this Metadata.Key events should have the format map[string]interface{}.
+// The second parameter is the metadata key where all the events will be aggregated.
+// The aggregation will with the format: map[string][]interface{}.
+// All events with the same key in the map will be joined in an slice.
+// To use this step we can use a graph with a time period context, eg: G.At(1479899809,3600).V().Events('A','B').
+// Or we can define the time period in the step: G.V().Events('A','B',1500000000,1500099999).
+// Note that in this case we define the start and end time, while in "At" is start time and duration.
+// In both cases, Events step will use the nodes given by the previous step.
 type EventsGremlinTraversalStep struct {
 	traversal.GremlinTraversalContext
 	EventKey    string
 	EventAggKey string
+	StartTime   time.Time
+	EndTime     time.Time
 }
 
 // NewEventsTraversalExtension returns a new graph traversal extension
@@ -47,6 +61,7 @@ func (e *EventsTraversalExtension) ParseStep(t traversal.Token, p traversal.Grem
 	}
 
 	var eventKey, eventAggKey string
+	var startTime, endTime time.Time
 	var ok bool
 
 	switch len(p.Params) {
@@ -59,14 +74,35 @@ func (e *EventsTraversalExtension) ParseStep(t traversal.Token, p traversal.Grem
 		if !ok {
 			return nil, errors.New("Events second parameter have to be a string")
 		}
+	case 4:
+		eventKey, ok = p.Params[0].(string)
+		if !ok {
+			return nil, errors.New("Events first parameter have to be a string")
+		}
+		eventAggKey, ok = p.Params[1].(string)
+		if !ok {
+			return nil, errors.New("Events second parameter have to be a string")
+		}
+		startTimeUnixEpoch, ok := p.Params[2].(int64)
+		if !ok {
+			return nil, errors.New("Events third parameter have to be a int (unix epoch time)")
+		}
+		startTime = time.Unix(startTimeUnixEpoch, 0)
+		endTimeUnixEpoch, ok := p.Params[3].(int64)
+		if !ok {
+			return nil, errors.New("Events fourth parameter have to be a int (unix epoch time)")
+		}
+		endTime = time.Unix(endTimeUnixEpoch, 0)
 	default:
-		return nil, errors.New("Events parameter must have two parameters")
+		return nil, errors.New("Events parameter must have two or four parameters (OriginKey, DestinationKey, StartTime, EndTime)")
 	}
 
 	return &EventsGremlinTraversalStep{
 		GremlinTraversalContext: p,
 		EventKey:                eventKey,
 		EventAggKey:             eventAggKey,
+		StartTime:               startTime,
+		EndTime:                 endTime,
 	}, nil
 }
 
@@ -74,7 +110,8 @@ func (e *EventsTraversalExtension) ParseStep(t traversal.Token, p traversal.Grem
 func (s *EventsGremlinTraversalStep) Exec(last traversal.GraphTraversalStep) (traversal.GraphTraversalStep, error) {
 	switch tv := last.(type) {
 	case *traversal.GraphTraversalV:
-		return InterfaceEvents(s.StepContext, tv, s.EventKey, s.EventAggKey), nil
+		return s.InterfaceEvents(tv)
+
 	}
 	return nil, traversal.ErrExecutionError
 }
@@ -93,16 +130,43 @@ func (s *EventsGremlinTraversalStep) Context() *traversal.GremlinTraversalContex
 // input nodes and put them into the newest node for each id into Metadata.aggKey.
 // Events are groupped based on its key. See mergedEvents for an example.
 // All output nodes will have Metadata.aggKey defined (empty or not).
-func InterfaceEvents(ctx traversal.StepContext, tv *traversal.GraphTraversalV, key, aggKey string) traversal.GraphTraversalStep {
-	it := ctx.PaginationRange.Iterator()
-	//gslice := tv.GraphTraversal.Graph.GetContext().TimeSlice
+func (s *EventsGremlinTraversalStep) InterfaceEvents(tv *traversal.GraphTraversalV) (traversal.GraphTraversalStep, error) {
+	it := s.StepContext.PaginationRange.Iterator()
+
+	// If user has defined start/end time in the parameters, use that values instead of the ones comming with the graph
+	if !s.StartTime.IsZero() && !s.EndTime.IsZero() {
+		timeSlice := graph.NewTimeSlice(
+			graph.Time(s.StartTime).UnixMilli(),
+			graph.Time(s.EndTime).UnixMilli(),
+		)
+		userTimeSliceCtx := graph.Context{
+			TimeSlice: timeSlice,
+			TimePoint: true,
+		}
+
+		newGraph, err := tv.GraphTraversal.Graph.CloneWithContext(userTimeSliceCtx)
+		if err != nil {
+			return nil, err
+		}
+		tv.GraphTraversal.Graph = newGraph
+	}
 
 	tv.GraphTraversal.RLock()
 	defer tv.GraphTraversal.RUnlock()
 
+	// uniqNodes store the latest node for each node identifier
 	uniqNodes := map[graph.Identifier]*graph.Node{}
+	uniqNodesLock := sync.Mutex{}
+
 	// events accumulate the events for each node id
 	events := map[graph.Identifier]map[string][]interface{}{}
+	eventsLock := sync.Mutex{}
+
+	// Limit the number of goroutines querying the backend at the same time
+	// A high number of concurrent queries makes ElasticSearch slower.
+	backendLimitConcurrentCalls := make(chan struct{}, 40)
+
+	wg := sync.WaitGroup{}
 
 	for _, node := range tv.GetNodes() {
 		// TODO afecta la paginación a esta función? Cuando se usa esta paginación?
@@ -110,32 +174,47 @@ func InterfaceEvents(ctx traversal.StepContext, tv *traversal.GraphTraversalV, k
 			break
 		}
 
-		// Get all revisions for this node
-		revisionNodes := tv.GraphTraversal.Graph.GetNodeAll(node.ID)
+		// Spawn a different goroutine for each node to speed up this function
+		// Elasticsearch client is thread safe.
+		wg.Add(1)
+		go func(nodeID graph.Identifier) {
+			defer wg.Done()
 
-		// Store only the most recent nodes
-		for _, rNode := range revisionNodes {
-			storedNode, ok := uniqNodes[rNode.ID]
-			if !ok {
-				uniqNodes[rNode.ID] = rNode
-			} else {
-				if storedNode.Revision < rNode.Revision {
+			// Get all revisions for this node
+			backendLimitConcurrentCalls <- struct{}{}
+			revisionNodes := tv.GraphTraversal.Graph.GetNodeAll(nodeID)
+			<-backendLimitConcurrentCalls
+
+			// Store only the most recent nodes
+			for _, rNode := range revisionNodes {
+				uniqNodesLock.Lock()
+				storedNode, ok := uniqNodes[rNode.ID]
+				if !ok {
 					uniqNodes[rNode.ID] = rNode
+				} else {
+					if storedNode.Revision < rNode.Revision {
+						uniqNodes[rNode.ID] = rNode
+					}
 				}
-			}
+				uniqNodesLock.Unlock()
 
-			// Store events from all revisions into the "events" variable
-			events[rNode.ID] = mergeEvents(rNode, key, events[rNode.ID])
-		}
+				// Store events from all revisions into the "events" variable
+				eventsLock.Lock()
+				events[rNode.ID] = mergeEvents(rNode, s.EventKey, events[rNode.ID])
+				eventsLock.Unlock()
+			}
+		}(node.ID)
 	}
+
+	wg.Wait()
 
 	// Move the nodes from the uniqNodes map to an slice required by TraversalV
 	nodes := []*graph.Node{}
 	for id, n := range uniqNodes {
 		e, ok := events[id]
 		if ok {
-			// Set the stored node with the merge of Alarms from previous and current node
-			metadataSet := uniqNodes[id].Metadata.SetField(aggKey, e)
+			// Set the stored node with the merge of Events from previous and current node
+			metadataSet := uniqNodes[id].Metadata.SetField(s.EventAggKey, e)
 			if !metadataSet {
 				logging.GetLogger().Errorf("Unable to set events metadata for host %v", id)
 			}
@@ -144,7 +223,7 @@ func InterfaceEvents(ctx traversal.StepContext, tv *traversal.GraphTraversalV, k
 		nodes = append(nodes, n)
 	}
 
-	return traversal.NewGraphTraversalV(tv.GraphTraversal, nodes)
+	return traversal.NewGraphTraversalV(tv.GraphTraversal, nodes), nil
 }
 
 // mergeEvents return the merge of node.Key events with the ones already stored in nodeEvents
